146d8a4a4b633367ffff7e8cbe8da8ea318e0f48
Mohnish <mohnish.a007@gmail.com>
2024-04-26 23:01:07 +0100

    Constant related comments

--- Constants/constant.c
+++ Constants/constant.c
@@ -3,8 +3,10 @@
 int main() {
    // printf() displays the string inside quotation
    // variables and data types in C
+   //int pie = 10.5 // we connot do this as pie is constant
    int num = 25;
    //const float pie = 3.14;
+   // pie = 10.7 // We cannot do this as pie is constant
    //printf("Hello, World! %d", num);
    printf("Value of float constant is %f\n", pie);
    return 0;
