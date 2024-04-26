770843499e5df651b4baf2009314a077388ad8e7
Mohnish <mohnish.a007@gmail.com>
2024-04-26 22:59:01 +0100

    Constant declare using #define preprocessor directive

--- Constants/constant.c
+++ Constants/constant.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#define pie 3.14
 int main() {
    // printf() displays the string inside quotation
    // variables and data types in C
    int num = 25;
-   const float pie = 3.14;
+   //const float pie = 3.14;
    //printf("Hello, World! %d", num);
    printf("Value of float constant is %f\n", pie);
    return 0;
