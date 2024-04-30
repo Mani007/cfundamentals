6047f0b27a937359281293b740a4c437a3adf84c
Mohnish <mohnish.a007@gmail.com>
2024-04-30 21:46:55 +0100

    some corner cases of for loop

--- Loops/forloop.c
+++ Loops/forloop.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 int main() {
    
-   int index=0, num;
+   int index=0, j=0, num;
   
     printf("Enter a number of time you want to repeat between 0 to 10 .... \n" );
     scanf("%d", &num);
     if (num>0 && num<10) 
         {
-    /* code */
-           for (index==0; index<num; index++)
+    //intial condition is optional if declared and assigned value above
+           for (; index<num, j<5; j++) // conditional expression can be multiple statements with zero or non zero values. 
           {
                     printf("We are in for loop with index at %d,\n", index);
                     //index++; Also written as index= index + 1  or index += 1
+                    // expression 3 in for loop is optional
           }
            
                 
