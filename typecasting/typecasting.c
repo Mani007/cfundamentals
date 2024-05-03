f1bb34e3747386906f97d56e6b0679197d518c48
Mohnish <mohnish.a007@gmail.com>
2024-05-03 03:18:57 +0100

    with typecasting division is working fine with comment

--- typecasting/typecasting.c
+++ typecasting/typecasting.c
@@ -2,6 +2,6 @@
 int main() {
     int a=3;
     float b=48;
-    printf("\tThe value of a divided by b is %f\n",(float)a/b);
+    printf("\tThe value of a divided by b is %f\n",(float)a/b); // typecasting is done
 return 0;
 }
