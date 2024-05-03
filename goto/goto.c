f8c031bdfcde5100fe0507c0a663a4fe881c077d
Mohnish <mohnish.a007@gmail.com>
2024-05-03 02:54:23 +0100

    breaking out of goto infinite loop with some modification

--- goto/goto.c
+++ goto/goto.c
@@ -8,8 +8,8 @@ int main() {
     // while loop to print 1 to 8
     top:   // Declaring a label called top. This may create infinite loops.  create infinite loop
     printf("We are inside goto statement.\n");
+     goto end;
     printf("We are inside INFINITE LOOP.\n");
-   goto end;
     goto top;
     end:
     printf("We have escaped out of goto loop.\n");
