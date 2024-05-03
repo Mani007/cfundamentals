c935b04e934e123e7ba82a5db448227a6938838f
Mohnish <mohnish.a007@gmail.com>
2024-05-03 03:02:08 +0100

    nested for loop before goto statement

--- goto/goto.c
+++ goto/goto.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 int main() {
    
-   //int index;
-   int i=0;
-  
-         
-    // while loop to print 1 to 8
-    top:   // Declaring a label called top. This may create infinite loops.  create infinite loop
-    printf("We are inside goto statement.\n");
-     goto end;
-    printf("We are inside INFINITE LOOP.\n");
-    goto top;
-    end:
-    printf("We have escaped out of goto loop.\n");
-   
+    //int i=0;
+      
+    // top:   // Declaring a label called top. This may create infinite loops.  create infinite loop
+    // printf("We are inside goto statement.\n");
+    //  goto end;
+    // printf("We are inside INFINITE LOOP.\n");
+    // goto top;
+    // end:
+    // printf("We have escaped out of goto loop.\n");
+   for (int i = 0; i < 5; i++)
+   {
+    printf(" We are at first loop I index at %d\n",i);
+    /* nested loop */
+     for (int j = 0; j < 5; j++)
+     {
+      printf(" We are at nested loop J index at %d\n",j);
+     
+    /* if(j==3){
+        goto endpart;
+    } */
+      }
+
+   }
+    endpart: 
+    printf("We have escaped out of all the loop.\n");
    
    return 0;
 }
