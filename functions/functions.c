d96308a23f52710961dd8fb8e34a446c4aac9b39
Mohnish <mohnish.a007@gmail.com>
2024-05-04 02:24:53 +0100

    Basic function code with comment

--- functions/functions.c
+++ functions/functions.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+int sun(int a,int b);  // Function declaration 
 
-int sum(int a, int b){
-    return a+b;
-}
 int main() {
     int a,b;
     printf("Enter two numbers: \n ");
     scanf("%d %d",&a,&b);
-    printf("%d",sum(a,b));
+    printf("%d",sum(a,b));   // Function call
     
 return 0;
+}
+
+int sum(int a, int b){   // Function defintion
+    return a+b;
 }
