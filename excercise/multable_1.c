df68e5532000efcf39a2ad095f54d5108594f00a
Mohnish <mohnish.a007@gmail.com>
2024-05-04 16:04:10 +0100

    Excercise1 multiplication table is solved improved

--- excercise/multable_1.c
+++ excercise/multable_1.c
@@ -4,7 +4,7 @@ int main() {
     printf("Please enter a number \n");
     scanf("%d", &num);
     printf("Multiplication Table for %d is \n ", num);
-    for (int i = 0; i < 10; i++)
+    for (int i = 1; i < 11; i++)
     {
         /* code */
         printf("%d * %d = %d \n", num, i, num*i);
