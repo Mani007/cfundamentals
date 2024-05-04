2a8957321de12e30fc9ea6285ba4a90428a910c4
Mohnish <mohnish.a007@gmail.com>
2024-05-04 03:39:06 +0100

    Basic function code with comment updated with star

--- functions/functions.c
+++ functions/functions.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 int sum(int a,int b);  // Function declaration 
-
+void printscr(int a); // Function declaration
 int main() {
     int a,b;
     printf("Enter two numbers: \n ");
     scanf("%d %d",&a,&b);
-    printf("%d",sum(a,b));   // Function call
+    printscr(10); 
+     printf("\n");  // Function call
+    printf("%d\n",sum(a,b));   // Function call
     
 return 0;
 }
 
 int sum(int a, int b){   // Function defintion
     return a+b;
+} 
+
+void printscr(int a){   // Function defintion
+   
+    for ( int i = 0; i < a; i++)
+    {
+        /* code */
+        printf("%c ",'*');
+    }
+    
 }
