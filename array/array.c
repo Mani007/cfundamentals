d01b7c768f948d71b0fea155cf5c506889cd3e91
Mohnish <mohnish.a007@gmail.com>
2024-05-06 22:11:02 +0100

    basic array setup basic changes

--- array/array.c
+++ array/array.c
@@ -3,5 +3,7 @@ int main() {
     int marks[4];
     marks[0]=43;
     printf("Marks of student zero is %d\n", marks[0]);
+    marks[0]=63;
+    printf("Marks of student zero NOW is %d\n", marks[0]);
 return 0;
 }
