e43991270393da97570fcb3e57fd129b845e20ae
Mohnish <mohnish.a007@gmail.com>
2024-05-15 00:48:04 +0100

    basic array looping  advance syntax 1d array

--- array/array.c
+++ array/array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int i;
-    int marks[4] = {25,56,67,77};
+    int marks[] = {25,56,67,77}; //declaration with initialization marks
     marks[0]=43;
     printf("Marks of student zero is %d\n", marks[0]);
     // marks[0]=63;
