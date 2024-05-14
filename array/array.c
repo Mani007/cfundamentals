8f67c9f7436ff1c4098732630ae095f201c146ea
Mohnish <mohnish.a007@gmail.com>
2024-05-15 00:46:53 +0100

    basic array looping  advance syntax

--- array/array.c
+++ array/array.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main() {
     int i;
-    int marks[4];
+    int marks[4] = {25,56,67,77};
     marks[0]=43;
     printf("Marks of student zero is %d\n", marks[0]);
     // marks[0]=63;
     // marks[1]=34;
     // marks[2]=56;
     // marks[3]=78;
-    for ( i = 0; i < 4; i++)
-    {
-        scanf("%d", &marks[i]);
-    }
+    // for ( i = 0; i < 4; i++)
+    // {
+    //     scanf("%d", &marks[i]);
+    // }
     
     for ( i = 0; i < 4; i++)
     {
