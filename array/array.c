baf57e97d6db557f90ab0188e60f46df316df206
Mohnish <mohnish.a007@gmail.com>
2024-05-15 00:44:32 +0100

    basic array looping with input

--- array/array.c
+++ array/array.c
@@ -4,10 +4,15 @@ int main() {
     int marks[4];
     marks[0]=43;
     printf("Marks of student zero is %d\n", marks[0]);
-    marks[0]=63;
-    marks[1]=34;
-    marks[2]=56;
-    marks[3]=78;
+    // marks[0]=63;
+    // marks[1]=34;
+    // marks[2]=56;
+    // marks[3]=78;
+    for ( i = 0; i < 4; i++)
+    {
+        scanf("%d", &marks[i]);
+    }
+    
     for ( i = 0; i < 4; i++)
     {
         printf("Marks of student %d is %d\n", i, marks[i]);
