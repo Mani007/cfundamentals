5a774ef0dc72d956a8201f15ab268d8e442b5af9
Mohnish <mohnish.a007@gmail.com>
2024-05-15 00:43:08 +0100

    basic array looping

--- array/array.c
+++ array/array.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 int main() {
+    int i;
     int marks[4];
     marks[0]=43;
     printf("Marks of student zero is %d\n", marks[0]);
     marks[0]=63;
-    printf("Marks of student zero NOW is %d\n", marks[0]);
+    marks[1]=34;
+    marks[2]=56;
+    marks[3]=78;
+    for ( i = 0; i < 4; i++)
+    {
+        printf("Marks of student %d is %d\n", i, marks[i]);
+    }
+    
+   // printf("Marks of student zero NOW is %d\n", marks[0]);
 return 0;
 }
