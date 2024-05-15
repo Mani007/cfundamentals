6e1617bba7b9fc4275147d32f44561be702edf68
Mohnish <mohnish.a007@gmail.com>
2024-05-15 01:03:28 +0100

    basic 2D array looping  advance syntax

--- array/array.c
+++ array/array.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 int main() {
-    int i;
-    int marks[] = {25,56,67,77}; //declaration with initialization marks
-    marks[0]=43;
-    printf("Marks of student zero is %d\n", marks[0]);
+    int i,j;
+    int marks[2][4] = {{25,56,67,77},
+                    {45,89,66,54}}; //declaration with initialization marks
     // marks[0]=63;
     // marks[1]=34;
     // marks[2]=56;
@@ -13,9 +12,14 @@ int main() {
     //     scanf("%d", &marks[i]);
     // }
     
-    for ( i = 0; i < 4; i++)
+    for ( i = 0; i < 2; i++)
     {
-        printf("Marks of student %d is %d\n", i, marks[i]);
+        //printf("Marks of student %d is %d\n", i, marks[i]);
+        for ( j = 0; j < 4; j++)
+        {
+            printf("Marks of subject [%d] [%d] is %d\n",i, j, marks[i][j]);
+        }
+        
     }
     
    // printf("Marks of student zero NOW is %d\n", marks[0]);
