1441ef440ffab9d3b30f544412253ebee2319cb8
Mohnish <mohnish.a007@gmail.com>
2024-05-16 19:48:33 +0100

    basic pointers details with dereferencing

--- pointers/pointer1.c
+++ pointers/pointer1.c
@@ -5,7 +5,7 @@ int main() {
     int *ptr; // integer pointer
     ptr = &var; // pointer to var 
     printf("integer value of  var is %d\n", var);
-    printf("integer value of  var is using pointer %x is %d\n", &var, var);
+    printf("integer value of  var is using pointer dereferencing operator at address %x is %d\n", &var, *ptr); // dereferencing the pointer prt
     printf("address of  var is %x\n", &var);
     printf("address of  pointer ptr is %x\n", &ptr);
   // %x is is format specifier for hexadecimal values
