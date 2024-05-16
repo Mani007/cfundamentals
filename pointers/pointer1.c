dbfa35ad907b21b0196da10af32b5bf57a489e86
Mohnish <mohnish.a007@gmail.com>
2024-05-16 20:20:43 +0100

    basic pointers types of pointers- null pointer in code

--- pointers/pointer1.c
+++ pointers/pointer1.c
@@ -2,12 +2,14 @@
 int main() {
     int var;
     var =20;
-    int *ptr; // integer pointer
+    int *ptr, *ptr2; // integer pointer
     ptr = &var; // pointer to var 
+    ptr2 = NULL; // null pointer
     printf("integer value of  var is %d\n", var);
     printf("integer value of  var is using pointer dereferencing operator at address %x is %d\n", &var, *ptr); // dereferencing the pointer prt
     printf("address of  var is %x\n", &var);
     printf("address of  pointer ptr is %x\n", &ptr);
+    printf("address of  pointer ptr2 - THE NULL pointer is %x\n", ptr2);
   // %x is is format specifier for hexadecimal values
 
 return 0;
