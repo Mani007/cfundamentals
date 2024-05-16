26fbeadf535e026343bfee76fafca8f4783427fc
Mohnish <mohnish.a007@gmail.com>
2024-05-16 19:47:16 +0100

    basic pointers details

--- pointers/pointer1.c
+++ pointers/pointer1.c
@@ -3,8 +3,12 @@ int main() {
     int var;
     var =20;
     int *ptr; // integer pointer
+    ptr = &var; // pointer to var 
     printf("integer value of  var is %d\n", var);
+    printf("integer value of  var is using pointer %x is %d\n", &var, var);
     printf("address of  var is %x\n", &var);
+    printf("address of  pointer ptr is %x\n", &ptr);
+  // %x is is format specifier for hexadecimal values
 
 return 0;
 }
