33697a8888895a6c565f7dbb20f74fa020b1f19e
Mohnish <mohnish.a007@gmail.com>
2024-05-17 23:58:51 +0100

    pointer arthmatic with arrays notes added

--- pointers/arrayspointers.c
+++ pointers/arrayspointers.c
@@ -6,7 +6,8 @@ int main() {
     
     printf("integer value of  var is %d\n", var[0]);
     printf("integer value of  var[0] is using pointer dereferencing operator at address %p is %d\n", ptr, *ptr); // dereferencing the pointer prt
-    printf("integer value of  var[1] is using pointer dereferencing operator and pointer arithmetic with addition at address of var[1] %p is %d\n", ptr+1, *(ptr+1)); // dereferencing the pointer prt
+    printf("integer value of  var[1] is using pointer dereferencing operator and pointer arithmetic with addition at address of var[1] %p is %d\n", ptr+1, *(ptr+1)); // dereferencing the pointer prt  
+    // var[i] == *(ptr+i) also var[i] == *(var+i)
     printf("address of  first value of array var is %p\n", ptr);
     printf("address of  pointer ptr is %p\n", &ptr); //this is the pointer address
     
