5d205b481393cbfbdf36f29f9ae46e83f00a5deb
Mohnish <mohnish.a007@gmail.com>
2024-05-18 01:03:19 +0100

    array advanced pointer arthmatic with notes

--- pointers/arrayspointers.c
+++ pointers/arrayspointers.c
@@ -9,7 +9,9 @@ int main() {
     printf("integer value of  var[1] is using pointer dereferencing operator and pointer arithmetic with addition at address of var[1] %p is %d\n", ptr+1, *(ptr+1)); // dereferencing the pointer prt  
     // var[i] == *(ptr+i) also var[i] == *(var+i)
     printf("address of  first value of array var is %p\n", ptr);
-    printf("address of  pointer ptr is %p\n", &ptr); //this is the pointer address
+    printf("address of  pointer ptr is %p\n", &ptr); //this is the pointer address 
+    // var++ will throw an error as var is an array with a constant pointer
+    // ptr++ is a valid statement
     
 return 0;
 }
