7c628e34339d3e7a4d875c8f6a8a45a3c1e2e7e5
Mohnish <mohnish.a007@gmail.com>
2024-05-19 01:24:11 +0100

    basic struct program more definations working code

--- structures/basicstruc.c
+++ structures/basicstruc.c
@@ -5,21 +5,26 @@ struct employee_data
     int id;
     char* name;
     int age;
-} employee;
+} emp1, emp2;
 
 int main() {
     //struct employee_data employee;
-    employee.id = 101;
-    employee.age = 25;
+    emp1.id = 101;
+    emp1.age = 25;
+    //emp2 = {emp2.id=102,emp2.age=45,emp2.name="xyz"};
+   struct employee_data emp2 = {emp2.id=102,emp2.name="xyz",emp2.age=45};
     // employee.name[0] = 'J';
     // employee.name[1] = 'h';
     // employee.name[2] = 'o';
     // employee.name[3] = 'n';
     // employee.name[4] = '\0';
-    employee.name = "abc";
-    printf("Employee ID is %d\n", employee.id);
-    printf("Employee age is %d\n", employee.age);
-    printf("Employee name is %s\n", employee.name);
+    emp1.name = "abc";
+    printf("Employee ID is %d\n", emp1.id);
+    printf("Employee age is %d\n", emp1.age);
+    printf("Employee name is %s\n", emp1.name);
+    printf("Employee ID is %d\n", emp2.id);
+    printf("Employee age is %d\n", emp2.age);
+    printf("Employee name is %s\n", emp2.name);
     
     
 return 0;
