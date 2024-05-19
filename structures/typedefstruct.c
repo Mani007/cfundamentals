6f3f61848310b51bcf15bf30b5665baec5f393f0
Mohnish <mohnish.a007@gmail.com>
2024-05-20 00:06:48 +0100

    nuances  typedef some more example working code

--- structures/typedefstruct.c
+++ structures/typedefstruct.c
@@ -11,8 +11,11 @@ typedef struct student {
 // defining new name for str1 
 typedef struct Foo fun; 
 int main() {
+    int* p,q; // note it means p is a pointer and q is an integer 
+    // to make both pointer, we use typedef
+    typedef int* intptr;
+    intptr ptr1,ptr2; // both ptr1 and ptr2 are integer pointer
     struct student X;
-    
     newstudent A;
     fun B;
     A.x = 1;
