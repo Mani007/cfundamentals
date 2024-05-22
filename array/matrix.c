5e06919a9347d29ceeb3db3d27e084f228aa1a74
Mohnish <mohnish.a007@gmail.com>
2024-05-22 18:45:21 +0100

    Matrix pointer first value

--- array/matrix.c
+++ array/matrix.c
@@ -1,17 +1,19 @@
 int main(int argc, char const *argv[])
 {
-    int a[5] = {0,1,2,3,4};
+    int a[3][3] = {{1,2,3},
+                   {4,5,6},
+                   {7,8,9} };
     int *p;
     p= a;
-    printf("Address of pointer p to array a[0] is %x\n",p); // address of initial value of array pointer
+    printf("Address of pointer p to array a[0][0] is %x\n",p); // address of initial value of array pointer
     printf("First value of pointer *p to array a is %d\n",*p); // 0
-    printf("First value of array a is %d\n",a[0]);  // 0
-    printf("Address of pointer p to array a[1] is %x\n",p+1);
-    printf("Second value of pointer *(p+1) to array a is %d\n",*(p+1)); // 1
-    printf("second value of array a is %d\n",a[1]);  // 1
-    printf("Address of pointer p to array a[2] is %x\n",p+2);
-    printf("Second value of pointer *(p+2) to array a is %d\n",*(p+2)); // 2
-    printf("second value of array a is %d\n",a[2]);  // 2
+    printf("First value of array a is %d\n",a[0][0]);  // 0
+    // printf("Address of pointer p to array a[1] is %x\n",p+1);
+    // printf("Second value of pointer *(p+1) to array a is %d\n",*(p+1)); // 1
+    // printf("second value of array a is %d\n",a[1]);  // 1
+    // printf("Address of pointer p to array a[2] is %x\n",p+2);
+    // printf("Second value of pointer *(p+2) to array a is %d\n",*(p+2)); // 2
+    // printf("second value of array a is %d\n",a[2]);  // 2
 
     return 0;
 }
