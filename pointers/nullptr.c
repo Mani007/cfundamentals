155d42f055690164c9313640856d0be4d7d34ae5
Mohnish <mohnish.a007@gmail.com>
2024-07-06 19:56:11 +0100

    NULL pointers uses in C

--- pointers/nullptr.c
+++ pointers/nullptr.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 int main() {
     int a=10;
-    int *ptr = NULL;
-    printf("The value of *ptr=NULL is  %d\n", ptr); // ptr =0
+    int *ptr = &a;
+    if (ptr != NULL) {
+        
+        printf("The value of *ptr=%d\n", *ptr); // ptr =10
+    }  else {
+        printf("ptr is NULL and cannot be dereferenced \n"); // ptr =0
+    }
+    //printf("The value of *ptr=NULL is  %d\n", ptr); // ptr =0
 return 0;
 }
