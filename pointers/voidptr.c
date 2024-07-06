9e98f103a1cbeff0155784387874d653f9b58ae7
Mohnish <mohnish.a007@gmail.com>
2024-07-06 18:09:30 +0100

    void pointers in C with updates

--- pointers/voidptr.c
+++ pointers/voidptr.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main() {
     int i = 10;
-    int j = 20;
+    float j = 20.10;
     void *ptr;
-    ptr = &i;
+    ptr = &j;
     //printf("The value stored in void pointer is %d\n", *ptr);
-    printf("The value stored in void pointer is %d\n", *( (int *)ptr) ); // dereference of void pointer using typecasting, also please pay attention to brackets. 
+    printf("The value stored in void pointer is %f\n", *( (float *)ptr) ); // dereference of void pointer using typecasting, also please pay attention to brackets. 
 return 0;
 }
