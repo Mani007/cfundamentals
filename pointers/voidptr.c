d0db0338d1a8a0a827ac07df4610092804606bf1
Mohnish <mohnish.a007@gmail.com>
2024-07-06 18:12:50 +0100

    void pointers in C with malloc

--- pointers/voidptr.c
+++ pointers/voidptr.c
@@ -6,5 +6,6 @@ int main() {
     ptr = &j;
     //printf("The value stored in void pointer is %d\n", *ptr);
     printf("The value stored in void pointer is %f\n", *( (float *)ptr) ); // dereference of void pointer using typecasting, also please pay attention to brackets. 
+    ptr = (int *)malloc(sizeof(int));  // malloc returns void pointer which will be typecasted into int type. 
 return 0;
 }
